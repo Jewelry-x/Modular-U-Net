9f4c5279071b4aae3a48022333ce20926db47779
Jewelry-x <deligiannisjerry@gmail.com>
2024-02-09 17:22:04 +0000

    Added ability to change numbers of workers to more than 0

--- model.py
+++ model.py
@@ -261,155 +261,141 @@ class NPyDataset(Dataset):
 
 
 ## training
-model = UNet(1,1)  # input 1-channel 3d volume and output 1-channel segmentation (a probability map)
-if use_cuda:
-    model.cuda()
+def train():
+    model = UNet(1,1)  # input 1-channel 3d volume and output 1-channel segmentation (a probability map)
+    if use_cuda:
+        model.cuda()
 
-# training data loader
-train_set = NPyDataset(folder_name)
-train_loader = DataLoader(
-    train_set,
-    batch_size=4,
-    shuffle=True,
-    num_workers=0)
-'''test
-dataiter = iter(train_loader)
-images, labels = dataiter.next()
-preds = model(images)
-'''
+    # training data loader
+    train_set = NPyDataset(folder_name)
+    train_loader = DataLoader(
+        train_set,
+        batch_size=4,
+        shuffle=True,
+        num_workers=0)
+    # test/validation data loader
+    test_set = NPyDataset(folder_name, is_train=False)
+    test_loader = DataLoader(
+        test_set,
+        batch_size=1,
+        shuffle=False,
+        num_workers=0)
 
-# test/validation data loader
-test_set = NPyDataset(folder_name, is_train=False)
-test_loader = DataLoader(
-    test_set,
-    batch_size=1,
-    shuffle=False,
-    num_workers=0)
-eva_loader = DataLoader(
-    train_set,
-    batch_size=4,
-    shuffle=True,
-    num_workers=0)
+    # optimisation loop
+    num_epochs=int(500)
+    best_eval_loss = 9999
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-2)
 
-# optimisation loop
-num_epochs=int(500)
-best_eval_loss = 9999
-optimizer = torch.optim.AdamW(model.parameters(), lr=1e-2)
+    if TRAIN:
+        os.makedirs("result", exist_ok=True)
+        workbook = Workbook()
+        worksheet = workbook.active
+        worksheet.append(['Epoch', 'Train Loss', 'Val Loss', 'Val IoU', 'Model Saved'])
 
-if TRAIN:
-    os.makedirs("result", exist_ok=True)
-    workbook = Workbook()
-    worksheet = workbook.active
-    worksheet.append(['Epoch', 'Train Loss', 'Val Loss', 'Val IoU', 'Model Saved'])
-
-    for epoch in range(num_epochs):
-        running_train_loss = 0.0
-        correct_train = 0
-        total_train = 0
-        train_iou = 0.0
-        
-        running_val_loss = 0.0
-        correct_val = 0
-        total_val = 0
-        
-        for ii, (images, labels) in enumerate(train_loader):
-            # step += 1
-            if use_cuda:
-                images, labels = images.cuda(), labels.cuda()
-
-            optimizer.zero_grad()
-            outputs = model(images)
-
-            loss = loss_2Ddice(outputs, labels)
-
-            loss.backward()
-            optimizer.step()
-            running_train_loss += loss.item()
-            iou,_ = calculate_average_metrics(np.squeeze(outputs.detach().cpu().numpy()), np.squeeze(labels.detach().cpu().numpy()))
-            pred_flat = outputs.view(-1)>0.5
-            target_flat = labels.view(-1)
-            total_train += target_flat.size(0)
-            train_iou +=iou
-            correct_train += (pred_flat == target_flat).sum().item()
+        for epoch in range(num_epochs):
+            running_train_loss = 0.0
+            correct_train = 0
+            total_train = 0
+            train_iou = 0.0
             
-        ## validation
-        y_pred_test=np.array([])
-        ytest=np.array([])
-        for i, (images, labels)  in enumerate(test_loader):
+            for (images, labels) in enumerate(train_loader):
+                # step += 1
+                if use_cuda:
+                    images, labels = images.cuda(), labels.cuda()
+
+                optimizer.zero_grad()
+                outputs = model(images)
+
+                loss = loss_2Ddice(outputs, labels)
+
+                loss.backward()
+                optimizer.step()
+                running_train_loss += loss.item()
+                iou,_ = calculate_average_metrics(np.squeeze(outputs.detach().cpu().numpy()), np.squeeze(labels.detach().cpu().numpy()))
+                pred_flat = outputs.view(-1)>0.5
+                target_flat = labels.view(-1)
+                total_train += target_flat.size(0)
+                train_iou +=iou
+                correct_train += (pred_flat == target_flat).sum().item()
+                
+            ## validation
+            y_pred_test=np.array([])
+            ytest=np.array([])
+            for i, (images, labels)  in enumerate(test_loader):
+                
+                images = images.cuda()
+                output = model(images)
+                # predicted = np.argmax(np.squeeze(output.detach().cpu().numpy()),axis=0)
+                # predicted = np.float32(np.squeeze(output.detach().cpu().numpy()))
+                predicted = np.float32(np.squeeze(output.detach().cpu().numpy(),axis=0)>0.5)
+                # filepath_to_save = os.path.join(RESULT_PATH,"label_test-pt.npy")
+                # np.save(filepath_to_save, predicted)
+                gt = np.squeeze(labels.detach().cpu().numpy(),axis=0)
+                if y_pred_test.size == 0:
+                    y_pred_test = predicted
+                    ytest = gt
+                else:
+                    y_pred_test = np.concatenate((y_pred_test, predicted),axis=0)
+                    ytest = np.concatenate((ytest, gt),axis=0)
+                
+            val_iou,dice = calculate_average_metrics(ytest,y_pred_test)
+            train_iou = train_iou/len(train_loader)
+            epoch_train_loss = running_train_loss / len(train_loader)
+            epoch_val_loss = 1-dice  
             
-            images = images.cuda()
-            output = model(images)
-            # predicted = np.argmax(np.squeeze(output.detach().cpu().numpy()),axis=0)
-            # predicted = np.float32(np.squeeze(output.detach().cpu().numpy()))
-            predicted = np.float32(np.squeeze(output.detach().cpu().numpy(),axis=0)>0.5)
-            # filepath_to_save = os.path.join(RESULT_PATH,"label_test-pt.npy")
-            # np.save(filepath_to_save, predicted)
-            gt = np.squeeze(labels.detach().cpu().numpy(),axis=0)
-            if y_pred_test.size == 0:
-                y_pred_test = predicted
-                ytest = gt
-            else:
-                y_pred_test = np.concatenate((y_pred_test, predicted),axis=0)
-                ytest = np.concatenate((ytest, gt),axis=0)
+            print(f'Epoch {epoch+1}, Train Loss: {epoch_train_loss:.4f}, Validation Loss: {epoch_val_loss:.4f}, Val iou: {val_iou:.2f}')
             
-        val_iou,dice = calculate_average_metrics(ytest,y_pred_test)
-        train_iou = train_iou/len(train_loader)
-        epoch_train_loss = running_train_loss / len(train_loader)
-        epoch_val_loss = 1-dice  
-        
-        print(f'Epoch {epoch+1}, Train Loss: {epoch_train_loss:.4f}, Validation Loss: {epoch_val_loss:.4f}, Val iou: {val_iou:.2f}')
-        
-        model_saved = False
+            model_saved = False
 
-        if epoch_val_loss<best_eval_loss:
-            model_saved = True
-            best_eval_loss = epoch_val_loss
-            torch.save(model, os.path.join(RESULT_PATH,'saved_' + type(optimizer).__name__ + '_model_pt'))
-            print("Model Saved Successfully") 
-        
-        update_excel(epoch+1, epoch_train_loss, epoch_val_loss, val_iou, model_saved, worksheet)
-        
-        # Save the workbook
-        workbook.save("./result/progress.xlsx")
-    print('Training done.')
+            if epoch_val_loss<best_eval_loss:
+                model_saved = True
+                best_eval_loss = epoch_val_loss
+                torch.save(model, os.path.join(RESULT_PATH,'saved_' + type(optimizer).__name__ + '_model_pt'))
+                print("Model Saved Successfully") 
+            
+            update_excel(epoch+1, epoch_train_loss, epoch_val_loss, val_iou, model_saved, worksheet)
+            
+            workbook.save("./result/progress.xlsx")
+        print('Training done.')
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model = torch.load(os.path.join(RESULT_PATH,'saved_' + type(optimizer).__name__ + '_model_pt'))
+    model = torch.load(os.path.join(RESULT_PATH,'saved_' + type(optimizer).__name__ + '_model_pt'))
 
-model = model.to(device)
-model.eval()
-y_pred_test=np.array([])
-ytest=np.array([])
-iou = []
-dice_coefficient = []
-for i, (images, labels)  in enumerate(test_loader):
-    # concat = torch.cat((x0, x1), 0)
-    \
-    images = images.cuda()
-    output = model(images)
-    # predicted = np.argmax(np.squeeze(output.detach().cpu().numpy()),axis=0)
-    # predicted = np.float32(np.squeeze(output.detach().cpu().numpy()))
-    predicted = np.float32(np.squeeze(output.detach().cpu().numpy(),axis=0)>0.5)
-    # filepath_to_save = os.path.join(RESULT_PATH,"label_test-pt.npy")
-    # np.save(filepath_to_save, predicted)
-    gt = np.squeeze(labels.detach().cpu().numpy(),axis=0)
-    if y_pred_test.size == 0:
-        y_pred_test = predicted
-        ytest = gt
-    else:
-        y_pred_test = np.concatenate((y_pred_test, predicted),axis=0)
-        ytest = np.concatenate((ytest, gt),axis=0)
+    model = model.to(device)
+    model.eval()
+    y_pred_test=np.array([])
+    ytest=np.array([])
+    iou = []
+    dice_coefficient = []
+    for i, (images, labels)  in enumerate(test_loader):
+        # concat = torch.cat((x0, x1), 0)
+        \
+        images = images.cuda()
+        output = model(images)
+        # predicted = np.argmax(np.squeeze(output.detach().cpu().numpy()),axis=0)
+        # predicted = np.float32(np.squeeze(output.detach().cpu().numpy()))
+        predicted = np.float32(np.squeeze(output.detach().cpu().numpy(),axis=0)>0.5)
+        # filepath_to_save = os.path.join(RESULT_PATH,"label_test-pt.npy")
+        # np.save(filepath_to_save, predicted)
+        gt = np.squeeze(labels.detach().cpu().numpy(),axis=0)
+        if y_pred_test.size == 0:
+            y_pred_test = predicted
+            ytest = gt
+        else:
+            y_pred_test = np.concatenate((y_pred_test, predicted),axis=0)
+            ytest = np.concatenate((ytest, gt),axis=0)
 
-    iou_val, dice_coefficient_val = calculate_2d_metrics(gt,predicted)
+        iou_val, dice_coefficient_val = calculate_2d_metrics(gt,predicted)
 
-    iou.append(iou_val)
-    dice_coefficient.append(dice_coefficient_val)
+        iou.append(iou_val)
+        dice_coefficient.append(dice_coefficient_val)
 
-# print("Average IOU is:" , sum(iou) / len(iou) )
-# print("Average DC is:" , sum(dice_coefficient) / len(dice_coefficient) )
-iou_val, dice_coefficient_val = calculate_average_metrics(ytest,y_pred_test)
-print("Final IOU:", iou_val)
-print("Final DC:", dice_coefficient_val)
+    # print("Average IOU is:" , sum(iou) / len(iou) )
+    # print("Average DC is:" , sum(dice_coefficient) / len(dice_coefficient) )
+    iou_val, dice_coefficient_val = calculate_average_metrics(ytest,y_pred_test)
+    print("Final IOU:", iou_val)
+    print("Final DC:", dice_coefficient_val)
 
-
+if __name__ == '__main__':
+    train()
